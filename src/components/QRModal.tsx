f238be100570ae557b7ae97be5a1ff2c7d9ef059
Emigrek <kaioshinbox@gmail.com>
2023-04-21 17:26:31 +0200

    moved QR code generation from web to external lib

--- src/components/QRModal.tsx
+++ src/components/QRModal.tsx
@@ -5,6 +5,7 @@ import useQRModal from '@/hooks/useQRModal';
 import { MdClose } from 'react-icons/md';
 import Image from 'next/image';
 import { VscLoading } from 'react-icons/vsc';
+import QRCode from "react-qr-code";
 
 const customStyles = {
     overlay: {
@@ -31,20 +32,12 @@ interface QRModalProps { }
 
 const QRModal: FC<QRModalProps> = () => {
     const { isOpen, setIsOpen, url } = useQRModal();
-    const [loading, setLoading] = useState<boolean>(true);
 
     return (
         <Modal isOpen={isOpen} style={customStyles} onRequestClose={() => setIsOpen(false)}>
             <div className="flex flex-col p-2 gap-5 items-center">
-                <div className='relative aspect-square w-80 md:w-96'>
-                    {
-                        loading ? (
-                            <div className='absolute inset-0 w-full h-full flex items-center justify-center bg-neutral-800 z-[51]'>
-                                <VscLoading className="animate-spin w-14 h-14 fill-white" />
-                            </div>
-                        ) : null
-                    }
-                    <Image onLoad={() => setLoading(false)} fill src={`https://api.qrserver.com/v1/create-qr-code/?size=400x400&data=${url}`} alt="QR Code" />
+                <div className='relative aspect-square'>
+                    <QRCode className='m-auto' value={url} size={300} bgColor="#262626" fgColor="#fff" />
                 </div>
                 <div className='flex-grow'>
                     <Button iconLeft={MdClose} onClick={() => setIsOpen(false)} variant={'red'}>Close</Button>
